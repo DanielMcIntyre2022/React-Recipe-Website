4a5017c3f88fc7eb6dca53593cb319a7d4418ee6
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-03-01 15:38:23 -0500

    created HomePage component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import SearchBar from './components/SearchBar';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
     <div className="App">
       <h1>Recipe Website</h1>
-      <SearchBar/>
+        <SearchBar/>
     </div>
   );
 }
