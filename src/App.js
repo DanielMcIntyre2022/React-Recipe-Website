4fd0d4c41a2c47ba8959f78cc8bef588d36d2e0f
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-03-01 11:54:06 -0500

    changed website title, get specific axios data object from api, imported useState and useEffect libraries to be used

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
 import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 const API_URL = `https://api.spoonacular.com/recipes/716429/information?apiKey=${process.env.REACT_APP_API_KEY}&includeNutrition=true`;
 
 axios.get(API_URL).then(res => {
-  console.log(res)
+  console.log(res.data)
 });
 
 function App() {
+
   return (
     <div className="App">
       <h1>Recipe Website</h1>
+      {/* Search Bar */}
     </div>
   );
 }
