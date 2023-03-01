73a4edfc859e2ae5284314f2f124206deb6c21d0
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-03-01 12:11:54 -0500

    initial setup of SearchBar component, imported SearchBar in main app file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import './App.css';
-import axios from 'axios';
-import { useState, useEffect } from 'react';
-
-const API_URL = `https://api.spoonacular.com/recipes/complexSearch?query=pasta&number=5&apiKey=${process.env.REACT_APP_API_KEY}&includeNutrition=true`;
-
-axios.get(API_URL).then(res => {
-  console.log(res.data)
-});
+import SearchBar from './components/SearchBar';
 
 function App() {
 
   return (
     <div className="App">
       <h1>Recipe Website</h1>
-      {/* Search Bar Component */}
-      {/* Display Search Results */}
+      <SearchBar/>
+      {/* Display Recipes Component */}
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,24 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function SearchBar() {
+
+const [ searchQuery, setSearchQuery ] = useState("");
+
+const API_URL = `https://api.spoonacular.com/recipes/complexSearch?query=pasta&number=5&apiKey=${process.env.REACT_APP_API_KEY}&includeNutrition=true`;
+
+    axios.get(API_URL).then(res => {
+      console.log(res.data)
+    });
+
   return (
-    <div>SearchBar</div>
+    <div>
+        <form>
+            <input
+            className='border'
+            />
+        </form>
+    </div>
   )
 }
 
