c3ae403e83a701bb780e9d8197b7bf842c51ad01
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-03-01 11:41:07 -0500

    successful initial test api response

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 import './App.css';
+import axios from 'axios';
+
+const API_URL = `https://api.spoonacular.com/recipes/716429/information?apiKey=${process.env.REACT_APP_API_KEY}&includeNutrition=true`;
+
+axios.get(API_URL).then(res => {
+  console.log(res)
+});
 
 function App() {
   return (
