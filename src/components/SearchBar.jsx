414f16968f5b04cc081bfba9ae1dd49dd3401160
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-03-01 13:07:01 -0500

    made api call dynamic to userSearchQueries in the input, stored in state

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,18 +5,31 @@ function SearchBar() {
 
 const [ searchQuery, setSearchQuery ] = useState("");
 
-const API_URL = `https://api.spoonacular.com/recipes/complexSearch?query=pasta&number=5&apiKey=${process.env.REACT_APP_API_KEY}&includeNutrition=true`;
+const API_URL = `https://api.spoonacular.com/recipes/complexSearch?query=${searchQuery}&number=5&apiKey=${process.env.REACT_APP_API_KEY}&includeNutrition=true`;
 
-    axios.get(API_URL).then(res => {
-      console.log(res.data)
-    });
+const getRecipes = () => {
+        axios.get(API_URL).then(res => {
+            console.log(res.data)
+    }).catch(err => {
+        console.log(err)
+    })
+};
 
+const onSubmit = (e) => {
+    e.preventDefault();
+}
+  
   return (
     <div>
-        <form>
+        <form onSubmit={onSubmit}>
             <input
+            value={searchQuery}
+            type="text"
+            placeholder='search for a recipe...'
+            onChange={(e) => setSearchQuery(e.target.value)}
             className='border'
             />
+            <button onClick={getRecipes}>Search</button>
         </form>
     </div>
   )
