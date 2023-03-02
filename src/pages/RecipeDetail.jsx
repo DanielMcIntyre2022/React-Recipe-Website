e600f9e92cc59ddfc4203f624c3dfc1de00c7fd2
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-03-02 11:29:38 -0500

    mapped through and displayed data or recipe details on RecipeDetail page component

--- src/pages/RecipeDetail.jsx
+++ src/pages/RecipeDetail.jsx
@@ -11,8 +11,8 @@ function RecipeDetail( ) {
 
   // make axios call with the unique id as a param to receive data related to its ID
 
-  const getReceiptDetails = (uid) => {
-      axios.get(`https://api.spoonacular.com/recipes/${uid}/information/?apiKey=${process.env.REACT_APP_API_KEY}`)
+  const getReceiptDetails = async (uid) => {
+      await axios.get(`https://api.spoonacular.com/recipes/${uid}/information/?apiKey=${process.env.REACT_APP_API_KEY}`)
       .then(res => {
         setRecipeDetails(res.data)
       })
@@ -23,11 +23,36 @@ function RecipeDetail( ) {
     getReceiptDetails(uniqueID)
   }, [uniqueID]);
 
+  console.log(recipeDetails)
+
   return (
     <div>
       <div>
         <h2>{recipeDetails.title}</h2>
         <img src={recipeDetails.image}/>
+      </div>
+      <div className="health-info">
+        <h2>Health Information:</h2>
+        <span>Vegetarian:{recipeDetails.vegetarian === true ? <p>Yes</p> : <p>No</p>}</span>
+        <span>Gluten Free:{recipeDetails.glutenFree === true ? <p>Gluten Free</p> : <p>No</p>}</span>
+        <span>Dairy Free:{recipeDetails.dairyFree === true ? <p>Diary Free</p> : <p>No</p>}</span>
+        <span>Very Healthy?{recipeDetails.veryHealthy === true ? <p>Diary Free</p> : <p>No</p>}</span>
+      </div>
+      <div className="ingredient-list">
+        <h2>Ingredients:</h2>
+        { 
+        recipeDetails.extendedIngredients &&
+        recipeDetails.extendedIngredients.map(ingredient => (
+          <p key={ingredient}>{ingredient.name}</p>
+        ))}
+      </div>
+      <div className="cooking-instructions">
+        <h2>Instructions:</h2>
+          {
+          recipeDetails.extendedIngredients &&
+          recipeDetails.analyzedInstructions[0].steps.map(instructions => (
+            <p key={instructions}>{instructions.step}</p>
+          ))}
       </div>
     </div>
   )
