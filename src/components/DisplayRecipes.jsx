706cc335571027acf37e7880983d3fab0fc7bb0d
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-03-02 15:00:48 -0500

    addditonal styling for DisplayRecipes components

--- src/components/DisplayRecipes.jsx
+++ src/components/DisplayRecipes.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { GrLinkNext, GrLinkPrevious } from 'react-icons/gr';
 
 function DisplayRecipes( {recipeResults}) {
 
@@ -16,39 +17,43 @@ const selectPageHandler = (selectPage) => {
 
   return (
     // Display Recipes //
+<div>
     <div className="display-recipes sm:grid sm:grid-cols-2 
-    lg:grid-cols-3 xl:grid-cols-4 2x1:grid-cols-5 max-w-6x1 
+    lg:grid-cols-3 xl:grid-cols-3 2x1:grid-cols-2 max-w-6x1 
     mx-auto py-4 ">
         {recipeResults.slice(page * 5 - 5, page * 5).map(recipe => (
-            <div key={recipe}>
-                <h2>{recipe.title}</h2>
+            <div key={recipe} className="flex m-2">
                 <Link to={`recipe/${recipe.id}`}>
-                   <img src={recipe.image} alt='recipe image'/>
+                   <img className="rounded-full border-8" src={recipe.image} alt='recipe image'/>
                 </Link>
+                <h2 className="flex justify-center items-center ml-2 font-semibold">{recipe.title}</h2>
             </div>
         ))}
+        </div>
         {
     // Pagination Logic //
             recipeResults.length > 0 && 
             <div className="pagination"> 
                 <button
-                onClick={() => selectPageHandler(page - 1)}
-                className="">previous</button>
+                onClick={() => selectPageHandler(page - 1)}>
+                    <GrLinkPrevious className="w-10 h-5"/>
+                </button>
 
                 {[...Array(recipeResults.length / 5)].map((_,i) => {
                         return <span 
-                        className={page === i+1? "bg-gray-300" : ""}
+                        className={page === i+1? "bg-gray-300" : ""} 
                         onClick={() => selectPageHandler(i + 1)} key={i}>
                             {i +1}
                         </span>
                     })}
 
                 <button 
-                onClick={() => selectPageHandler(page + 1)}
-                className="">next</button>
+                onClick={() => selectPageHandler(page + 1)}>
+                    <GrLinkNext className="w-10 h-5"/>
+                </button>
             </div>
         }
-    </div>
+</div>
   )
 };
 
