315793efb623c4ca653bd355c4eb76b800742ffc
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-03-02 11:56:54 -0500

    added styles for Home component page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,13 @@ import SearchBar from '../components/SearchBar'
 
 function Home() {
   return (
-    <div className='flex'>
+    <div className='flex justify-between mx-16 max-w-6xl mt-10'>
+      <div className='flex'>
         <SearchBar/>
+      </div>
+      <div className='flex'>
         <CuisineFilter/>
+     </div>
     </div>
   )
 }
