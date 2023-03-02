9901eb2d2f68422997bdc94c838b72e796ef335c
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-03-02 13:08:24 -0500

    created handleFilter function to get the value of the selected cuisine option

--- src/components/CuisineFilter.jsx
+++ src/components/CuisineFilter.jsx
@@ -3,13 +3,23 @@ import { useState } from "react";
 
 function CuisineFilter() {
 
-const [ cuisineFilter, setCuisineFilter ] = useState()
+const [ cuisineFilter, setCuisineFilter ] = useState('')
 
 const API_URL = `https://api.spoonacular.com/recipes/complexSearch?query=${cuisineFilter}&apiKey=${process.env.REACT_APP_API_KEY}&includeNutrition=true`;
 
+const handleFilter = (e) => {
+    const valueSelected = e.target.value;
+    setCuisineFilter(valueSelected)
+};
+
+console.log(cuisineFilter);
+
   return (
     <div className="filter">
-        <select>
+        <select 
+        className="p-2"
+        name="cuisine" 
+        onChange={handleFilter}>
             {
                 cuisines.map(cuisine => (
                     <option>{cuisine}</option>
