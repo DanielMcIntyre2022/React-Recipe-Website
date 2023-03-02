ddc032efff9fc16e7e966204ed564824a8e6c6c7
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-03-02 12:27:06 -0500

    created data file with an array of the cusine param options, imported and mapped through the options in the CuisineFilter component

--- src/components/CuisineFilter.jsx
+++ src/components/CuisineFilter.jsx
@@ -1,10 +1,23 @@
-
-import React from 'react'
+import { cuisines } from "../data";
+import { useState } from "react";
 
 function CuisineFilter() {
+
+const [ cuisineFilter, setCuisineFilter ] = useState()
+
+const API_URL = `https://api.spoonacular.com/recipes/complexSearch?query=${cuisineFilter}&apiKey=${process.env.REACT_APP_API_KEY}&includeNutrition=true`;
+
   return (
-    <div>CuisineFilter</div>
+    <div className="filter">
+        <select>
+            {
+                cuisines.map(cuisine => (
+                    <option>{cuisine}</option>
+                ))
+            }
+        </select>
+    </div>
   )
-}
+};
 
 export default CuisineFilter;
