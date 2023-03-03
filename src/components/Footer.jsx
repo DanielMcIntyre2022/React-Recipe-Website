287b80214619591a5cc3b135b0e42a3eb547c55c
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-03-02 20:37:34 -0500

    updated styles on Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 
 function Footer() {
   return (
-    <div className="footer bg-teal-300 p-5 fixed inset-x-0 bottom-0">
+    <div className="footer bg-teal-300 p-2 fixed inset-x-0 bottom-0">
         <p>Created By Daniel McIntyre</p>
     </div>
   )
