dddc4a0a207afea43b029572565956fcfbbd83c8
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-03-02 20:03:51 -0500

    added additional styles to RecipeDetail page component

--- src/pages/RecipeDetail.jsx
+++ src/pages/RecipeDetail.jsx
@@ -26,37 +26,42 @@ function RecipeDetail( ) {
   return (
   <div className="recipe-detail-container mt-10 flex flex-col">
     <div className="recipe-detail-wrapper flex">
+      <div className="flex flex-col w-80%">
       <div className="left-recipe-detail flex">
         <div className="recipe-image-title">
-          <img className="border-8 rounded-full" src={recipeDetails.image}/>
-          <h2 className="font-bold">{recipeDetails.title}</h2>
+          <img className="rounded-full" src={recipeDetails.image}/>
+          <h2 className="font-bold mt-5 mb-5">{recipeDetails.title}</h2>
         </div>
       </div>
-      <div className="health-info flex flex-col items-center justify-center border">
-        <h2 className="font-semibold underline">Health Information:</h2>
+      <div className="health-info flex flex-col border p-10">
+        <h2 className="font-semibold underline mb-5">Health Information:</h2>
         <span>Vegetarian: {recipeDetails.vegetarian === true ? <span>Yes</span> : <span>No</span>}</span>
-        <span>Gluten Free:{recipeDetails.glutenFree === true ? <span>Gluten Free</span> : <span>No</span>}</span>
-        <span>Dairy Free:{recipeDetails.dairyFree === true ? <span>Diary Free</span> : <span>No</span>}</span>
-        <span>Very Healthy?{recipeDetails.veryHealthy === true ? <span>Diary Free</span> : <span>No</span>}</span>
+        <span>Gluten Free: {recipeDetails.glutenFree === true ? <span>Gluten Free</span> : <span>No</span>}</span>
+        <span>Dairy Free: {recipeDetails.dairyFree === true ? <span>Diary Free</span> : <span>No</span>}</span>
+        <span>Very Healthy? {recipeDetails.veryHealthy === true ? <span>Diary Free</span> : <span>No</span>}</span>
       </div>
-      <div className="ingredient-list border">
-        <h2 className="font-semibold underline mb-5">Ingredients:</h2>
+      </div>
+      <div className="ingredient-list border p-10 w-50%">
+        <h2 className="font-semibold underline mb-5 text-xl">Ingredients:</h2>
+        <ul>
         { 
         recipeDetails.extendedIngredients &&
         recipeDetails.extendedIngredients.map(ingredient => (
-          <span key={ingredient}>{ingredient.name}</span>
+             <li key={ingredient}>{ingredient.name}</li>
         ))}
+        </ul>
         </div>
-      </div>
-      <div className="cooking-instructions border">
-        <h2 className="font-semibold underline">Instructions:</h2>
+      <div className="cooking-instructions border pl-5 pr-5">
+        <h2 className="font-semibold underline mb-5 mt-10 text-xl">Cooking Instructions:</h2>
+        <ol>
           {
           recipeDetails.extendedIngredients &&
           recipeDetails.analyzedInstructions[0].steps.map(instructions => (
-            <span className="flex flex-col mt-5" key={instructions}>{instructions.step}</span>
+              <li key={instructions}>{instructions.step}</li>
           ))}
+          </ol>
       </div>
-     
+      </div>
     </div>
   )
 }
