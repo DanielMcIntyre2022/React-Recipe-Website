a19944b4bdbd8af65b434fbcadfba6ebad25cb38
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-03-02 21:06:47 -0500

    added additional styling to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
 import { Link } from "react-router-dom";
+import { MdOutlineFoodBank } from 'react-icons/md';
 
 function Header() {
   return (
-    <div className="header bg-teal-100 p-5">
-        <Link to='/'>
+    <div className="header bg-teal-100 p-5 flex justify-center item-center">
+          <div className="app-logo">
+            <MdOutlineFoodBank size={40} className="mr-5"/>
+          </div>
+          <div className="app-title mt-2">
+          <Link to='/'>
             <h1 className="text-lg font-semibold">Recipe Website</h1>
         </Link>
+        </div>
     </div>
   )
 }
